9cf1e399ac2f17ae7593915225095bbc0831d556
Dhanesh Kumar <dmeghwar.bscs18seecs@seecs.edu.pk>
2022-10-31 23:03:02 +0500

    Header: header component contain title and logo of memegenrator

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <header className='header'>
+            <img
+                src='/images/meme2.png'
+                className='header-image'>
+            </img>
+            <h1
+                className='header-title'>
+                Meme Machine
+            </h1>
+        </header>
+    )
+}
